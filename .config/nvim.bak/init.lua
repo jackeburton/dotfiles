3302b4285eb1f43fef14dffc57265439446fd28a
Jack Burton <jackeburton97@gmail.com>
2023-11-19 16:05:01 +0000

    adding .config folder

---
+++ .config/nvim.bak/init.lua
@@ -0,0 +1,2 @@
+require('jogburt')
+print('hello')
---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,8 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+--
+vim.o.expandtab = false -- Use tabs instead of spaces
+vim.o.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for
+vim.o.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
+vim.o.softtabstop = 4 -- Number of spaces that a <Tab> counts for while performing editing operations
